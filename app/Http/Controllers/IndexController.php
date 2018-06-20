832938ee0d1b18f97eb52b6d89063f184c25026f
m0sg <m0sgsergey@gmail.com>
2018-06-20 21:28:07 +0300

    commit

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,13 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\About;
+use App\Advantages;
+use App\Products;
+use App\Reviews;
+use App\Sliders;
 
 class IndexController extends Controller
 {
 
-    public function execute (Request $request) {
+    public function execute(Request $request)
+    {
 
-        return view('layouts.site');
+        $abouts = about::all();
+        $advantages = advantages::all();
+        $products = products::all();
+        $reviews = reviews::all();
+        $sliders = sliders::all();
+
+
+
+
+
+        return view('site.index');
     }
 
 }
---
+++ resources/views/site/header.blade.php
@@ -0,0 +1,34 @@
+<nav class="navbar">
+    <div class="container">
+        <div class="navbar-header">
+
+            <!-- Mobile Toggle -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- / Mobile Toggle -->
+
+            <!-- Logo  -->
+            <h1 class="logo">
+                <a href="#">Martin Luther</a>
+            </h1>
+            <!-- / Logo  -->
+        </div>
+        <!-- Navbar -->
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="current"><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#resume">Resume</a></li>
+                <li><a href="#our-work">Our Work</a></li>
+                <li><a href="#blog">Blog</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+        <!-- / Navbar -->
+    </div>
+</nav>
